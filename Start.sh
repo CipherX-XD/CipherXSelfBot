4802280721926668214b08fa5a4b59ff5a0c5f20
CipherX-XD <76248151+CipherX-XD@users.noreply.github.com>
2021-03-20 20:11:30 +0330

    Create Start.sh

---
+++ Start.sh
@@ -0,0 +1,52 @@
+echo "
+   _______       __             _  __
+  / ____(_)___  / /_  ___  ____| |/ /
+ / /   / / __ \/ __ \/ _ \/ ___/   /
+/ /___/ / /_/ / / / /  __/ /  /   |
+\____/_/ .___/_/ /_/\___/_/  /_/|_|
+      /_/
+	    °•° Process Begins •°•
+	    °•° Happy Norouz 💐 •°•
+	    °•° Support : @FutureTechnologyGuardX •°•
+"
+echo '
+        •• Getting Packages and Installing
+'
+
+export TZ=Asia/Tehran 
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+apt update && apt upgrade -y && apt install --no-install-recommends -y \
+    python \
+    bash \
+    curl \
+    git \
+
+echo '
+        •• Cloning Repository
+'
+git clone https://github.com/CipherX-XD/CipherXSelfBot.git /root/CipherX-XD/
+
+ 
+echo '
+	•• Getting Libraries and Installing
+' 
+pip install --upgrade pip wheel 
+
+pip install -r /root/CipherX-XD/requirements.txt
+ 
+echo "
+             _______       __             _  __
+            / ____(_)___  / /_  ___  ____| |/ /
+           / /   / / __ \/ __ \/ _ \/ ___/   /
+          / /___/ / /_/ / / / /  __/ /  /   |
+          \____/_/ .___/_/ /_/\___/_/  /_/|_|
+              /_/
+			•°• Installation Successfully °•°
+		   •• Wait till python images are pushed
+	   •• Give build logs to Support Group if installation fails
+"
+echo '
+	•• Running the Self Bot
+'
+python3 -m cipherx
